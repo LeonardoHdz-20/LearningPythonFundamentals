5c01ff3e004470fd3499d9ed5945b0ddbcdd9b35
LeonardoHdz-20 <134025927+LeonardoHdz-20@users.noreply.github.com>
2025-01-25 01:13:32 -0600

    Update lists.py

--- clase2/lists.py
+++ clase2/lists.py
@@ -1,1 +1,33 @@
+print("Manejo de listas en PYTHON\n\n")
 
+# Declaración de una lista
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f"Valores actuales de mi lista: {my_list}")
+# Agregar elementos a mi lista con append, 0 y 11
+my_list.append(0)
+my_list.append(11)
+
+print(f"Valores modificados de mi lista: {my_list}")
+
+# Ordenar los elementos de mi lista mediante SORT
+my_list.sort()
+
+print(f"Los elementos de mi lista han sido ordenados: {my_list}")
+
+# Modificar y eliminar un elemento de mi lista
+my_list[0] = "Está es una cadena"
+my_list.pop()
+
+print(f"Valores actuales de mi lista, ejemplo POP: {my_list}")
+
+# Creación de una sublista [0:5]
+my_sublist = my_list[0:5]
+print(f"Valores actuales de mi sublista: {my_sublist}")
+
+# Imprimir los valores de mi lista
+print("\nEstos son los elementos de mi lista, los cuales serán impresos mediante un ciclo for")
+count = 0
+for item in my_list:
+    print(f"Elemento de mi lista {count} - {item}")
+    count += 1
